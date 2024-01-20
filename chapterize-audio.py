a1fd628f5547dcb2f11e66cfc5fafcfc4f189917
Valentin Bartschies <valentin.bartschies@tuhh.de>
2024-01-20 18:07:41 +0100

    make storing cue and json the default choice

--- chapterize-audio.py
+++ chapterize-audio.py
@@ -219,7 +219,7 @@ if __name__ == "__main__":
     if transcribing:
         if not prompt_yes_no(question="Use transcriptions as chapter lables?", default="yes"):
             transcript_labels = False
-    if prompt_yes_no(question="Store cue file?", default="no"):
+    if prompt_yes_no(question="Store cue file?", default="yes"):
         export_to_cue(silence_spots, input_audio_file)
-    if prompt_yes_no(question="Store chapter.json?", default="no"):
+    if prompt_yes_no(question="Store chapter.json?", default="yes"):
         export_to_json(silence_spots, input_audio_file)
